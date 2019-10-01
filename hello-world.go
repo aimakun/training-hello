706178b420492e34f8c8954c93f73240e1ab216c
Ubuntu <ubuntu@ip-172-31-10-82.ap-southeast-1.compute.internal>
2019-10-01 18:32:20 +0000

    Add change for CircleCI build test (without fix test code)

--- hello-world.go
+++ hello-world.go
@@ -11,7 +11,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "World"
+		target = "World CircleCI"
 	}
 	fmt.Fprintf(w, "Hello %s!", target)
 }
